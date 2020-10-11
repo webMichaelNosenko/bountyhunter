b002e24d3678d6cea6b9d218d6cb842e77e5b90c
webMichaelNosenko <mialex2013.am@gmail.com>
2020-10-12 01:19:57 +0400

    Getting actual program pages on hackerone, not parsing

--- bountyhunter.py
+++ bountyhunter.py
@@ -2,6 +2,18 @@ import re
 import requests
 import json
 import pymongo
+import pyppeteer
+import asyncio
+
+
+async def look_for_scope(handle):
+    browser = await pyppeteer.launch(headless=True, args=['--no-sandbox'])
+    page = await browser.newPage()
+    await page.goto('https://hackerone.com/' + handle + '?type=team')
+    await page.setViewport({'width': 1912, 'height': 933})
+    inner_html = await page.evaluate('document.body.textContent', force_expr=True)
+    print(inner_html)
+    await browser.close()
 
 hackerone = 'https://hackerone.com/programs/search?query=bounties%3Ayes&sort=name%3Aascending&limit=1000'
 bounty_object = {
@@ -42,7 +54,15 @@ h1headers = {
 }
 
 session = requests.Session()
-# programlist = session.get(hackerone, headers=h1headers)
 programlist = session.get(hackerone)
 bountiespage = json.loads(programlist.text)
-print('fugg')
+for placeholdObj in bountiespage['results']:
+    bounty_object['curr_handle'] = placeholdObj['handle']
+    print(bounty_object['curr_handle'])
+    asyncio.get_event_loop().run_until_complete(look_for_scope(bounty_object['curr_handle']))
+
+
+
+
+
+
